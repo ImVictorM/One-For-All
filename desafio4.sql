8e12579f2a06e4b0051a73d393b1ef3a5600aedd
Victor Mendes <victorutdsim7@gmail.com>
2022-12-01 12:31:26 -0300

    [REQ 4 - COMPLETO] feat: consulta usuarios ativos e inativos

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+SELECT 
+	`user`.full_name AS usuario,
+    IF(
+		MAX(YEAR(played_date))  >= 2021,
+      'Usuário ativo',
+      'Usuário inativo'
+	) AS status_usuario
+FROM SpotifyClone.`user` AS `user`
+INNER JOIN SpotifyClone.played_history AS played_history
+	ON played_history.user_id = `user`.id
+GROUP BY usuario
+ORDER BY usuario;
